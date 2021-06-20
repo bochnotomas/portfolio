20c2299b7f860698e13ef72717572f187846779f
bochnotomas <tomasbochnovic76@gmail.com>
2021-06-20 13:46:46 +0100

    i guess, css for hero section is done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import HeroSection from './components/HeroSection/HeroSection';
 import AboutMe from './components/AboutMe/AboutMe';
+import Nav from './components/Nav/Nav';
 
 function App() {
-  return (
-    <div className="App">
-        <HeroSection />
-        <AboutMe />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Nav />
+            <div className="main">
+                <HeroSection />
+                <AboutMe />
+            </div>
+            
+        </div>
+    );
 }
 
 export default App;
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Nav from '../Nav/Nav';
 import Intro from '../Intro/Intro';
 import styles from './HeroSection.module.css';
 
 export default function HeroSection() {
     return (
         <div className={styles.container}>
-            <Nav />
             <Intro />
         </div>
     )
